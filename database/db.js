e92a5a4eb1ed490c1f86281ab37a178f8a59f475
Gopika2772 <gopikaks.cse2020@citchennai.net>
2024-05-27 22:25:41 +0530

    env

--- database/db.js
+++ database/db.js
@@ -1,20 +1,20 @@
-// const mysql = require("mysql2");
-// const db = mysql.createConnection({
-//     host: "bl64f08yn5ybfwpaxjrj-mysql.services.clever-cloud.com",
-//     database: "bl64f08yn5ybfwpaxjrj",
-//     user: "uvbkybbay37tbeiw",
-//     password: "1iN7zP5CQ4jGPeUXwCP0",
-// });
-// module.exports = db
-
-
 const mysql = require("mysql2");
 const db = mysql.createConnection({
-    host: "localhost",
-    database: "rent",
-    user: "root",
-    password: "Gopika01#",
+    host: "bl64f08yn5ybfwpaxjrj-mysql.services.clever-cloud.com",
+    database: "bl64f08yn5ybfwpaxjrj",
+    user: "uvbkybbay37tbeiw",
+    password: "1iN7zP5CQ4jGPeUXwCP0",
 });
+module.exports = db
+
+
+// const mysql = require("mysql2");
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     database: "rent",
+//     user: "root",
+//     password: "Gopika01#",
+// });
 
 db.connect((err) => {
     if (err) {
